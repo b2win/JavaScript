af480dc652e1e89da5571f26bf4ddce160edfa5f
b2win <68144560+b2win@users.noreply.github.com>
2020-08-20 01:21:22 +0900

    2020-08-19 2400
    JavaScript Basics for Beginners 실습 진행중

---
+++ index.js
@@ -0,0 +1,44 @@
+// Performing a task
+// function greet(name) {
+//     console.log('Hello ' + name + '' + lastName);
+// }
+
+// greet('John', 'Smith');
+
+
+//Calculating a value
+// function square(number) {
+//     return number * number;
+// }
+
+// let number = square(2);
+// console.log(number);
+
+let x = 10;
+let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
+
+// Increment (++)
+console.log(x);
+console.log(++x);
+console.log(x++); // x++ => x = x + 1
+
+
+// Decrement (--)
+console.log(--x);
+
+
+//If a customer has more than 100 points,
+//they are a 'gold' customer
+//otherwise, they are a 'silver' customer
+
+let points = 110;
+let type = points > 100 ? 'gold' : 'silver';
+
+console.log(type);
